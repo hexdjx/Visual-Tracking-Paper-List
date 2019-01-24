2beec57d8bc573133777052d40f18a790ccfffe0
hexdjx <849459832@qq.com>
2019-01-24 13:01:14 +0800

    ECO_HC uses HOG and CN features

---
+++ ECO_HC/setup_paths.m
@@ -0,0 +1,34 @@
+function setup_paths()
+
+% Add the neccesary paths
+
+[pathstr, name, ext] = fileparts(mfilename('fullpath'));
+
+% Tracker implementation
+addpath(genpath([pathstr '/implementation/']));
+
+% Runfiles
+addpath([pathstr '/runfiles/']);
+
+% Utilities
+addpath([pathstr '/utils/']);
+
+% The feature extraction
+addpath(genpath([pathstr '/feature_extraction/']));
+
+% Matconvnet
+addpath([pathstr '/external_libs/matconvnet/matlab/mex/']);
+addpath([pathstr '/external_libs/matconvnet/matlab']);
+addpath([pathstr '/external_libs/matconvnet/matlab/simplenn']);
+
+% PDollar toolbox
+addpath(genpath([pathstr '/external_libs/pdollar_toolbox/channels']));
+
+% Mtimesx
+addpath([pathstr '/external_libs/mtimesx/']);
+
+% mexResize
+addpath([pathstr '/external_libs/mexResize/']);
+
+%VOT_integration
+addpath([pathstr '/VOT_integration/benchmark_wrapper/']);
