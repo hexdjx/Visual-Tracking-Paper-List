f3441a2d8951ce9913debf44125308ec11436d71
hexdjx <849459832@qq.com>
2019-01-24 12:52:46 +0800

    ECO no GPU

---
+++ ECO/demo_ECO.m
@@ -0,0 +1,14 @@
+
+% This demo script runs the ECO tracker with deep features on the
+% included "Crossing" video.
+
+% Add paths
+setup_paths();
+
+base_path = 'D:\\data_seq/OTB-100/';% D://data_seq/
+video_path = choose_video(base_path);
+% video_path = 'sequences/Crossing';
+[seq, ground_truth] = load_video_info(base_path,video_path);
+
+% Run ECO
+results = testing_ECO(seq);
